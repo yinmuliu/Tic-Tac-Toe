6f2b0f19b5e9e4f5f932a880a7f3090cee7e041c
Yinmu Liu <yinmu.liu@gmail.com>
2022-06-20 15:30:10 +1000

    add switch players function

--- script.js
+++ script.js
@@ -1,27 +1,58 @@
+// get the elements needed to work on
+const blocks = document.querySelectorAll('.block'); //return an array of blocks
+const playersTurn = document.querySelector('.playersTurn')
+const announceWin = document.querySelector('.announcer');
+const restartButton = document.querySelector('.game-restart');
+
+
+// define the initial variables - so they are ready for change in later functions
+let currentPlayer = 'Player X'
+let board = ['','','','','','','','','']
+
 // put X & O in the block when click
 
-// define blocks as an array of blocks in the gameboard
-const blocks = document.querySelectorAll('.block') //return an array of blocks
-// when a block is clicked, change the background image of the clicked block
 // create a function to identify which block is clicked.
 const clickBlock = (clickBlockEvent) => {
     let clickedBlock = clickBlockEvent.target;
     let clickedBlockIndex = clickedBlock.dataset['blockIndex'];
-    console.log(clickedBlock.dataset);
-    console.log(clickedBlockIndex);
+    // when clicked, pop up individual signs for each player
+    if (currentPlayer === 'Player X') {
+        clickedBlock.innerHTML = 'X';
+    } else {
+        clickedBlock.innerHTML = 'O';
+    }
+    // switch to another player
+    switchPlayer();
 }
 
-// create a for loop to add event handler for each block
-for (let i of blocks) {
-    i.addEventListener('click', clickBlock)
+// create a for loop to add event handler for each block, so they respond to clicks
+for (let block of blocks) {
+    block.addEventListener('click', clickBlock)
 }
 
+// create a function to change player after each click
+const switchPlayer = () => {
+    if (currentPlayer === 'Player X') {
+        currentPlayer = 'Player O'
+        playersTurn.innerHTML = 'Player O'
+    } else {
+        currentPlayer = 'Player X'
+        playersTurn.innerHTML = 'Player X'
+    }
+}
 
-// when click block 0, change the background image in block 0
+// create a win condition array
+const winCondition = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
 
-// change X & O when players take turns
-
-// create a win condition
 // 1. when a.3 signs in 1-4-8, 2-4-6, 0-3-6, 1-4-7, 2-5-8, 0-1-2, 3-4-5, 6-7-8
 // it is a win
 // create a function called isWin returns boolean
