977f7d1ced66997da31ea4e94bd5f34b63c4daa8
Yinmu Liu <yinmu.liu@gmail.com>
2022-06-22 15:58:02 +1000

    add checkFilled condition and cursor

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const blocks = document.querySelectorAll('.block'); //return an array of blocks
 const playersTurn = document.querySelector('.playersTurn') // for displaying player's turn
 const winMessage = document.querySelector('.announcer');
-const restartButton = document.querySelector('.game-restart');
+const hiddenResultPage = document.getElementById('resultpage');
+const playAgainButton = document.querySelector('.playagain');
 
 
 // define the initial variables - so they are ready for change in later functions
@@ -10,36 +11,47 @@ let currentPlayer = 'Player X'
 let indexFilled = {};
 
 // create a function for actions after the blocks are clicked
-const handleClick = (clickBlockEvent) => {
+const handleClick = (clickBlockEvent) => { 
     let clickedBlock = clickBlockEvent.target;
     let clickedBlockIndex = clickedBlock.dataset['blockIndex'];
-
-    // Place sign
-    placeSign(currentPlayer, clickedBlock)
-    // Check for win
-    checkWin(clickedBlockIndex)
-    // Switch turn
-    switchPlayer();
     
-    // if (checkWin(clickedBlockIndex)) {
-    //     endGame()
-    // } else {
-    //     switchPlayer()
-    // }
+    // check if the block is filled
+    if (clickedBlock.innerHTML === '') {
+        // if empty, can place sign and then set cursor to not allowed
+        placeSign(currentPlayer, clickedBlock)
+        clickedBlock.style.cursor = 'not-allowed'
+    } else {
+        // else if it is not clicked, set the cursor to pointer
+        clickedBlock.style.cursor = 'pointer'
+    }
+
+    // check if there is a winner/a tie
+    if (checkWin(clickedBlockIndex)) {
+        showResult()
+    } else {
+        switchPlayer()
+    }
 }
 
 // add event handler for each block, so they respond to clicks only once
 for (let block of blocks) {
-    block.addEventListener('click', handleClick, {once: true})
+    block.addEventListener('click', handleClick)
 }
 
 
 // place a sign in the block when it is clicked
 const placeSign = (player, clickedBlock) => {
     if (player === 'Player X') {
-        clickedBlock.innerHTML = 'X';
+        let catPaw = document.createElement('img');
+        catPaw.src = './images/img-cat.jpeg';
+        catPaw.width = 120;
+        clickedBlock.appendChild(catPaw);
+        
     } else if (player === 'Player O') {
-        clickedBlock.innerHTML = 'O';
+        let dogPaw = document.createElement('img');
+        dogPaw.src = './images/img-dog.jpeg';
+        dogPaw.width = 120;
+        clickedBlock.appendChild(dogPaw);
     }
 }
 
@@ -83,19 +95,19 @@ const checkWin = (index) => {
         if (indexFilled[move0] === 'Player X' && indexFilled[move1] === 'Player X' && indexFilled[move2] === 'Player X') {
             // when player X fill the right block index combination, it wins
             winMessage.innerHTML = `Player X wins!`
-            winMessage.style.visibility = 'visible'
+            // winMessage.style.visibility = 'visible'
             return true
         } else if (indexFilled[move0] === 'Player O' && indexFilled[move1] === 'Player O' && indexFilled[move2] === 'Player O') {
             // when player O fill the right block index combination, it wins
             winMessage.innerHTML = `Player O wins!`
-            winMessage.style.visibility = 'visible'
+            // winMessage.style.visibility = 'visible'
             return true
         } 
     }
     // create a condition to check tie: when all blocks has been filled but no winning condition is satisfied
     if (Object.keys(indexFilled).length === 9) {
         winMessage.innerHTML = `It's a tie!`
-        winMessage.style.visibility = 'visible'
+        // winMessage.style.visibility = 'visible'
         return true
     } else {
         return false
@@ -103,6 +115,33 @@ const checkWin = (index) => {
 }
 
 // end the game when there is a game result
-const endGame = () => {
-    
+const showResult = () => {
+    // pop up hidden result section - display change to block
+    hiddenResultPage.style.display = 'block';
+    // add effect on popping up
 }
+
+
+// play the game again - when the "play again" button is clicked
+const playAgain = () => {
+    // hide result page
+    hiddenResultPage.style.display = 'none';
+    
+    for (let block of blocks) {
+        // clear the board
+        block.innerHTML = ''
+        // reset cursor to pointer
+        block.style.cursor = 'pointer'
+    }
+    // reset object to empty
+    indexFilled = {}
+    
+    // add score to the winner
+        // if winner = x, score increment by 1
+        // if winner = o, score increment by 1
+        // if tie, no score increment
+    // player need to be able to put signs in again!!!
+    // add a startGame() function
+}
+playAgainButton.addEventListener('click', playAgain);
+
